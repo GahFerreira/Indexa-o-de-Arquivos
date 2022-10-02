5d013e36b936d2a39bff9fe365f91bce70f284a1
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2022-10-02 16:57:29 -0300

    Update Manipulador.cpp

--- Manipulador.cpp
+++ Manipulador.cpp
@@ -43,3 +43,155 @@ bool Manipulador::criar_arquivo_inicial(const char *nome_arq_entrada)
 
     return true;
 }
+
+
+
+
+//IDEIA PRA FUNÇÃO
+
+/*
+
+TituloNetflix* Manipulador::gerar_array_de_titulos(const char *nome_arq_entrada){
+	
+	fstream arq_entrada;
+	arq_entrada.open(nome_arq_entrada, ios_base::in);
+	
+	TituloNetflix titulos[8000];
+	
+	int contador_registro = 0;
+	
+	char c;
+	
+	while(arq_entrada.eof()){
+		
+		while(c != '\n'){
+		
+			contador_registro++;
+		
+			string tam_registro ="";
+		
+		
+			//supondo o tamanho máximo do campo que indica o tamanho igual a 6
+			for(int contador=0; contador<6;contador++){
+				
+				c=arq_entrada.get();
+				
+				tam_registro += c;
+			
+			}		
+		
+			//converter o valor do tamanho para int
+			int tam_registro_int = stoi(tam_registro);
+			
+			
+			//salva a informação do tamanho ocupado por aquele campo específico
+			titulos[contador_registro].tamanho_registro = tam_registro_int;
+			
+			for(int contador = 0;contador < titulos[contador_registro].tamanho_registro;contador++){
+				
+				//CADA WHILE PEGA UM CAMPO E PASSA PARA O RESPECTIVO ATRIBUTO DE TituloNetflix
+					
+				while(c != ';'){
+                
+                //primeiro campo...
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+                
+                //segundo campo...
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				while(c != ';'){
+					
+					
+					
+					
+				}
+				
+				
+				
+			}
+		
+		}
+		
+	}
+	
+	
+	
+}
+
+*/
+
+
+
